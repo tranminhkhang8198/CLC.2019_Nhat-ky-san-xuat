482fde67a23cb072248778efc7c8c42174fecfe6
noat <nmtoan251998@gmail.com>
2019-12-13 08:18:26 +0700

    improvement: add options to mongodb connection to fix deprecated api

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
-const {MongoClient} = require('mongodb');
-const {mongodbUrl, dbName} = require('./config')
+const { MongoClient } = require('mongodb');
+const { mongodbUrl, dbName } = require('./config');
+const options = { useUnifiedTopology: true };
 
 
 
 exports.connect = (cb) => {
 
-    MongoClient.connect(mongodbUrl, (err, client) => {
+    MongoClient.connect(mongodbUrl, options, (err, client) => {
 
         return cb(err, client);
 
