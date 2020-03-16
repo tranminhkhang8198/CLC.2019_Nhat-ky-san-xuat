845934d3d5de8966492518938a662d6a301a43bb
vanloiidk <vanloiidk@gmail.com>
2020-03-16 19:23:08 +0700

    add validation folder

--- backend/controllers/test.controller.js
+++ backend/controllers/test.controller.js
@@ -13,7 +13,13 @@ module.exports.testController = async (req, res, next) => {
             models,
         } = req.app;
         const result = await models.test.postNewTest(testObj);
-
+        if (result.length <= 0) {
+            return res, status(httpStatus.NOT_FOUND)
+                .json({
+                    conde: httpStatus.NOT_FOUND,
+                    message: 'test is not found',
+                })
+        }
         return res.status(httpStatus.OK)
             .json({
                 code: httpStatus.OK,
--- backend/routes/v1/test.router.js
+++ backend/routes/v1/test.router.js
@@ -3,8 +3,12 @@ const Router = require('express').Router();
 const {
     testController,
 } = require('../../controllers/test.controller');
+const {
+    validateBeforeInsert,
+} = require('../../validations/test.validation')
 
 Router.route('/').post(
+    validateBeforeInsert,
     testController,
 );
 
